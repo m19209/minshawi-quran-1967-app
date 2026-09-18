0dcd01279f79664ec74fd99f60aebd10a8895bd3
m19209 <m19209@users.noreply.github.com>
2026-09-18 22:13:16 +0300

    chore: bump version to 1.3.3 and configure build release workflow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.minshawi.quran1967"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.3.2"
+        versionCode = 16
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
